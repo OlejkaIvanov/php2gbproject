44842d506c3f55c474ef2cbf974103db76c3f704
Olejkaivanov <olejkaivanov90@gmail.com>
2018-10-02 17:16:04 +0300

    master
    zalil

---
+++ php2.uni.local/public/index.php
@@ -0,0 +1,7 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php";
+include $_SERVER['DOCUMENT_ROOT'] . "/../vendor/autoload.php";
+$config = include $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php" ;
+\app\base\App::call()->run($config);
+
+
